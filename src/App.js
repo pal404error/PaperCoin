de58a3517b559a2ca4dfb4dcfac7753c339fa50b
Pal <82200759+pal404error@users.noreply.github.com>
2024-03-03 15:00:49 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import LogIn from './components/login';
+import Forgot from './components/forgot';
+import { Route, Routes, useRoutes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Template from './components/template';
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="login" element={<Template/>}/>
+      <Route exact path="" element={<Template/>}/>
+      <Route exact path="reset" element={<Forgot/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
